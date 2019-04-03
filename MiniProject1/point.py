00b5c0afbe97b3b45b3fcab5c6559431c5a0b433
jramm <jrammer101@gmail.com>
2019-04-02 20:18:19 -0700

    Corrected __str__ function

--- MiniProject1/point.py
+++ MiniProject1/point.py
@@ -9,25 +9,28 @@ Credits: [N/A]
 # TODO Description
 """
 
+
 class Point:
+    """Point class that has x and y coordinates"""
 
     def __init__(self, x: int, y: int):
+        """Create x and y values"""
+
         self.x = x
         self.y = y
 
     def move(self, dx: int, dy: int):
+        """Move x by dx value and y by dy value"""
+
         self.x += dx
         self.y += dy
 
     def __eq__(self, other: "Point") -> bool:
+        """Checks to see if the coordinate of multiple points are the same"""
+
         return self.x == other.x and self.y == other.y
 
     def __str__(self):
-        print(f"{self.x}, {self.y}")
+        """Print the coordinate of the point"""
 
-p = Point(1, 2)
-# p.move(1, 1)
-p.__str__()
-# print(p.x, p.y)
-
-
+        return f"({self.x}, {self.y})"
