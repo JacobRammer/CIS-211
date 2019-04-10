b3804b785a794fa0dc4e3828376a94879230a68c
jramm <jrammer101@gmail.com>
2019-04-10 09:32:27 -0700

    Fixed max_run

--- MiniProject2/sweep.py
+++ MiniProject2/sweep.py
@@ -40,23 +40,20 @@ def dedup(l: list) -> list:
 def max_run(l: list) -> int:
     """Return the number of the longest consecutive character occurrence in a list"""
 
-    longest = 0
-    l_copy = l.copy()
-    l_copy.sort()
-    max_list = [] #
+    longest_run = 0
+    l_copy = l.copy()  # don't change original list
+    max_list = []
 
-    if len(l) != 0:
-        for i in l_copy:
-            if len(l_copy) <= longest and longest == 0:
-                longest = 1
-                break
+    if len(l) == 0 or len(l) == 1:
+        return len(l)
+    for i in l_copy:
+        max_list.append(l_copy[0])
+        l_copy.pop(0)
+        while len(l_copy) >= 1 and max_list[0] == l_copy[0]:
             max_list.append(l_copy[0])
-            l_copy.pop(l[0])
-            while max_list[0] in l_copy:
-                max_list.append(l_copy[0])
-                l_copy.pop(0)
-            if len(max_list) > longest:
-                longest = len(max_list)
-                max_list.clear()
+            l_copy.pop(0)
+        if len(max_list) >= longest_run:
+            longest_run = len(max_list)
+            max_list.clear()
 
-    return longest
+    return longest_run
