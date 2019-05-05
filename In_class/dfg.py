283a130400cfd4803240d44b57d38805419e29c4
jramm <jrammer101@gmail.com>
2019-05-04 21:57:21 -0700

    Blegh

--- In_class/dfg.py
+++ In_class/dfg.py
@@ -43,75 +43,95 @@
 from typing import List
 
 
-class Point(object):
-    def __init__(self, x: int, y: int):
-        self.x = x
-        self.y = y
+#
+#
+# class Point(object):
+#     def __init__(self, x: int, y: int):
+#         self.x = x
+#         self.y = y
+#
+#     def move(self, dx: int, dy: int):
+#         self.x += dx
+#         self.y += dy
+#
+#     def __str__(self):
+#         return "Point({}, {})".format(self.x, self.y)
+#
+#
+# class Rect(object):
+#     def __init__(self, ll: Point, width: int, height: int):
+#         self.ll = ll
+#         self.height = height
+#         self.width = width
+#
+#     def move(self, dx: int, dy: int):
+#         self.ll.move(dx, dy)
+#
+#     def ur(self) -> Point:
+#         return Point(self.ll.x + self.width, self.ll.y + self.height)
+#
+#     def __str__(self):
+#         return "Rect({}, {})".format(self.ll, self.ur())
+#
+#
+# corner = Point(10, 10)
+# small_rect = Rect(corner, 10, 10)
+# larger_rect = Rect(corner, 20, 20)
+# larger_rect.move(20, 20)
+# print("Smaller rect is {}".format(small_rect))
+# print("Larger rect is {}".format(larger_rect))
+#
+#
+# class NumberGrid(object):
+#     """A square grid of numbers"""
+#
+#     def __init__(self, values: List[List[int]]):
+#         self.values = values
+#         for row in values:
+#             assert len(row) == len(values)  # Grid is square
+#
+#     def is_balanced(self) -> bool:
+#
+#         """"Balanced if sum of each row and column is equal"""
+#         target = 0
+#         for item in self.values[0]:
+#             target += item
+#             # Each row the same
+#         for row in self.values:
+#             total = 0
+#             for item in row:
+#                 total += item
+#             if total != target:
+#                 return False
+#                 # Each column the same
+#         for col in range(len(self.values)):
+#             total = 0
+#             for row in range(len(self.values)):
+#                 total += self.values[row][col]
+#             if total != target:
+#                 return False
+#
+#         return True
+#
+#
+# assert NumberGrid([[2, 7, 6], [9, 5, 1], [4, 3, 8]]).is_balanced()
+# assert not (NumberGrid([[2, 7, 6], [9, 7, 1], [4, 3, 8]]).is_balanced())
+# print("Passed two simple test cases on 3x3 grid")
 
-    def move(self, dx: int, dy: int):
-        self.x += dx
-        self.y += dy
-
-    def __str__(self):
-        return "Point({}, {})".format(self.x, self.y)
-
-
-class Rect(object):
-    def __init__(self, ll: Point, width: int, height: int):
-        self.ll = ll
-        self.height = height
-        self.width = width
-
-    def move(self, dx: int, dy: int):
-        self.ll.move(dx, dy)
-
-    def ur(self) -> Point:
-        return Point(self.ll.x + self.width, self.ll.y + self.height)
-
-    def __str__(self):
-        return "Rect({}, {})".format(self.ll, self.ur())
+def every_column_zero(matrix: List[List[int]]) -> bool:
+    """matrix is 4 lists of 4 integers (4 rows of 4 columns each).
+    Return True iff each column has at least one element with value 0.
+    """
+    for col in range(len(matrix)):
+        has_zero = False
+        for row in matrix:
+            test = row[col]
+            if row[col] == 0:
+                has_zero = True
+        if not has_zero:
+            return False
+    return True
 
 
-corner = Point(10, 10)
-small_rect = Rect(corner, 10, 10)
-larger_rect = Rect(corner, 20, 20)
-larger_rect.move(20, 20)
-print("Smaller rect is {}".format(small_rect))
-print("Larger rect is {}".format(larger_rect))
-
-
-class NumberGrid(object):
-    """A square grid of numbers"""
-
-    def __init__(self, values: List[List[int]]):
-        self.values = values
-        for row in values:
-            assert len(row) == len(values)  # Grid is square
-
-    def is_balanced(self) -> bool:
-
-        """"Balanced if sum of each row and column is equal"""
-        target = 0
-        for item in self.values[0]:
-            target += item
-            # Each row the same
-        for row in self.values:
-            total = 0
-            for item in row:
-                total += item
-            if total != target:
-                return False
-                # Each column the same
-        for col in range(len(self.values)):
-            total = 0
-            for row in range(len(self.values)):
-                total += self.values[row][col]
-            if total != target:
-                return False
-
-        return True
-
-
-assert NumberGrid([[2, 7, 6], [9, 5, 1], [4, 3, 8]]).is_balanced()
-assert not (NumberGrid([[2, 7, 6], [9, 7, 1], [4, 3, 8]]).is_balanced())
-print("Passed two simple test cases on 3x3 grid")
+assert every_column_zero([[1, 0, 1, 0], [0, 1, 1, 1], [1, 1, 1, 1], [1, 1, 0, 1]])
+assert not every_column_zero([[1, 0, 1, 0], [1, 1, 1, 0], [1, 0, 1, 0], [1, 0, 0, 0]])
